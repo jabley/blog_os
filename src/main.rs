e084ff702d0a1b013d10a65b5fc78e9c164c9728
James Abley <james.abley@gmail.com>
2022-06-28 10:23:21 +0100

    Fix clippy warning
    Clippy rightly highlights our empty loop which will burn the CPU. We
    know this; it's by design at this stage.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use core::panic::PanicInfo;
 mod vga_buffer;
 
 #[no_mangle] // don't mangle the name of this function
+#[allow(clippy::empty_loop)] // yes this is bad, but we are aware of it and it's only for bootstrapping our implementation
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
