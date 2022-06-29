f5895cbbe5ab65cf7d79257c6ad9ae7687709e3d
James Abley <james.abley@gmail.com>
2022-06-29 10:48:30 +0100

    Show test failures on the console too
    We have a separate panic handler for the OS versus when we're running
    tests. This allows us to write data about test failures to the serial
    port too.
    
    We also add a failing test here which demonstrates the new feature.
    
    ```
    > cargo test
    ...
    Running 1 tests
    trivial assertion... [failed]
    
    Error: panicked at 'assertion failed: `(left == right)`
      left: `0`,
     right: `1`', src/main.rs:72:5
    ```

--- src/main.rs
+++ src/main.rs
@@ -23,9 +23,21 @@ pub extern "C" fn _start() -> ! {
 }
 
 /// This function is called on panic.
+// our existing panic handler
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
+    loop {}
+}
+
+// our panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
@@ -57,6 +69,6 @@ fn test_runner(tests: &[&dyn Fn()]) {
 #[test_case]
 fn trivial_assertion() {
     serial_print!("trivial assertion... ");
-    assert_eq!(1, 1);
+    assert_eq!(0, 1);
     serial_println!("[ok]");
 }
