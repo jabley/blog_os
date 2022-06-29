d318b5ed99825dc793c503a604e8b1eab11ade25
James Abley <james.abley@gmail.com>
2022-06-29 11:35:49 +0100

    Create a library
    Move a lot of things out of main.rs and into lib.rs. This is preparation
    work for having integration tests.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 #![no_std] // Don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 #![feature(custom_test_frameworks)] // replace the default test runner
-#![test_runner(crate::test_runner)] // with the test_runner function below
+#![test_runner(blog_os::test_runner)] // with the test_runner function below
 #![reexport_test_harness_main = "test_main"]
 
+use blog_os::println;
 use core::panic::PanicInfo;
-
-mod serial;
-mod vga_buffer;
 
 #[no_mangle] // don't mangle the name of this function
 #[allow(clippy::empty_loop)] // yes this is bad, but we are aware of it and it's only for bootstrapping our implementation
@@ -31,57 +29,8 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
-// our panic handler in test mode
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+    blog_os::test_panic_handler(info)
 }
