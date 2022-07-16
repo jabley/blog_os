9f65eaf814aac89447fca3c4144eae915d650a59
James Abley <james.abley@gmail.com>
2022-07-16 13:30:38 -0400

    Add hardware interrupt support
    We introduce this using the 8259 Programmable Interrupt Controller (PIC)
    interface. This is an older standard which is still supported for
    backward-compatability reasons. There is of course an associated crate
    which provides this functionality.
    
    Hardware interrupts occur asynchronously, so they are completely
    independent from the executed code and can occur at any time. Thus we
    have a source of concurrency in our kernel now.
    
    By default, PICs send interrupt vector numbers in the range 0 to 15 to
    the CPU. These numbers are already occupied by CPU exceptions; for
    example, 8 corresponds to a double fault. The first 32 interrupt vector
    numbers are for CPU exceptions, so we remap the PIC interrupts to
    different numbers. Here, we use 32 as the starting point.
    
    We add a primary and a secondary PIC, as `ChainedPics`. This will be
    eventually be configured something like:
    
    ```                     ____________                          ____________
    Real Time Clock --> |            |   Timer -------------> |            |
    ACPI -------------> |            |   Keyboard-----------> |            |      _____
    Available --------> | Secondary  |----------------------> | Primary    |     |     |
    Available --------> | Interrupt  |   Serial Port 2 -----> | Interrupt  |---> | CPU |
    Mouse ------------> | Controller |   Serial Port 1 -----> | Controller |     |_____|
    Co-Processor -----> |            |   Parallel Port 2/3 -> |            |
    Primary ATA ------> |            |   Floppy disk -------> |            |
    Secondary ATA ----> |____________|   Parallel Port 1----> |____________|
    ```
    
    We can then initialise the PICs and enable interrupts. We add a timer
    interrupt handler as our first example. The implementation is very
    similar to the existing CPU exception handlers.

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,10 +1,35 @@
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
-use crate::println;
+use crate::{print, println};
 
 use lazy_static::lazy_static;
 
 use crate::gdt;
+
+use pic8259::ChainedPics;
+use spin;
+
+pub const PIC_1_OFFSET: u8 = 32;
+pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+pub static PICS: spin::Mutex<ChainedPics> =
+    spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
+
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum InterruptIndex {
+    Timer = PIC_1_OFFSET,
+}
+
+impl InterruptIndex {
+    fn as_u8(self) -> u8 {
+        self as u8
+    }
+
+    fn as_usize(self) -> usize {
+        usize::from(self.as_u8())
+    }
+}
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
@@ -14,6 +39,8 @@ lazy_static! {
             idt.double_fault.set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX); // new
         }
+        idt[InterruptIndex::Timer.as_usize()]
+            .set_handler_fn(timer_interrupt_handler);
 
         idt
     };
@@ -32,6 +59,10 @@ extern "x86-interrupt" fn double_fault_handler(
     _error_code: u64,
 ) -> ! {
     panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    print!(".");
 }
 
 #[test_case]
