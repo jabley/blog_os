866dc5bc73fbf716baf508a69df93c4b5b6b59bf
James Abley <james.abley@gmail.com>
2022-07-29 09:46:07 +0100

    Create a frame allocator
    This shows how we can map pages that don't have a level 1 page table
    yet.
    
    It uses the memory map that is passed by the bootloader and assumes 4KiB
    page size.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 #![test_runner(blog_os::test_runner)] // with the test_runner function below
 #![reexport_test_harness_main = "test_main"]
 
-use blog_os::println;
+use blog_os::{memory::BootInfoFrameAllocator, println};
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 
@@ -20,10 +20,10 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    let mut frame_allocator = memory::EmptyFrameAllocator;
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
     // map an unused page
-    let page = Page::containing_address(VirtAddr::new(0));
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
     memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
     // write the string `New!` to the screen through the new mapping
